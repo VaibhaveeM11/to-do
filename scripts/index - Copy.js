220660d541125c2294a4f729cc4fc76085c115a7
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-07-16 16:27:10 +0530

    code added

--- scripts/index - Copy.js
+++ scripts/index - Copy.js
@@ -2,56 +2,43 @@
 //Feature: 1>Add Task 2>Remove Task 3>Edit Task
 let myNodelist = document.getElementsByTagName("LI");
 let i;
-for (i = 0; i < myNodelist.length; i++) 
-{
-
+for (i = 0; i < myNodelist.length; i++) {
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
   span.className = "close";
-
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
+// Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");
 let i;
-for (i = 0; i < close.length; i++) 
-{
+for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-   
     let div = this.parentElement;
     div.style.display = "none";
   }
 }
 
+// Add a "checked" symbol when clicking on a list item
 let list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
-//to add to list
 
+// Create a new list item when clicking on the "Add" button
 function newElement() {
-
-
   let li = document.createElement("li");
-
   let inputValue = document.getElementById("myInput").value;
-
   let t = document.createTextNode(inputValue);
   li.appendChild(t);
-
-  if (inputValue === '') 
-  {
-
+  if (inputValue === '') {
     alert("You must write something!");
-  } 
-  else 
-  {
+  } else {
     document.getElementById("myUL").appendChild(li);
   }
-
   document.getElementById("myInput").value = "";
 
   let span = document.createElement("SPAN");
@@ -60,17 +47,10 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-
-  for (i = 0; i < close.length; i++) 
-  {
-
-    close[i].onclick = function() 
-    {
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
       let div = this.parentElement;
       div.style.display = "none";
     }
-
   }
-
-
 }
